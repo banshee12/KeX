b00e1cf9befa8cfac5470f43fdc2536d5cf5e096
Jens Arth <Jens.Arth@outlook.de>
2024-01-29 09:28:48 +0100

    added SpringDoc OpenAPIDefinition

--- rest-api/src/main/java/ops/kex/restapi/Application.java
+++ rest-api/src/main/java/ops/kex/restapi/Application.java
@@ -1,5 +1,6 @@
 package ops.kex.restapi;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import ops.kex.restapi.model.Tags;
 import ops.kex.restapi.repository.TagsRepository;
 import org.springframework.boot.CommandLineRunner;
@@ -8,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@OpenAPIDefinition
 public class Application {
 
 	public static void main(String[] args) {
