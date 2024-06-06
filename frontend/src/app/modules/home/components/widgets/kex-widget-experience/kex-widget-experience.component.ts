538011c4945e45426e255f9ed1dae6aed982df8a
ottohartel <otto.haertel@gmail.com>
2024-06-06 22:31:07 +0200

    CI by Haertel* Adding Experience Widgets to Home Page

--- frontend/src/app/modules/home/components/widgets/kex-widget-experience/kex-widget-experience.component.ts
+++ frontend/src/app/modules/home/components/widgets/kex-widget-experience/kex-widget-experience.component.ts
@@ -1,7 +1,12 @@
 import { Component } from '@angular/core';
 import {Store} from "@ngrx/store";
+import {Observable, Subscription} from "rxjs";
 import {KexProfileState} from "../../../../profile/store/kex-profile.state";
+import {KexProfileSelector} from "../../../../profile/store/selectors/kex-profile.selectors";
+import {GetExperiencesActions} from "../../../../profile/store/actions/kex-profile.actions";
 import {Router} from "@angular/router";
+import {Experience} from "../../../../profile/models/kex-profile.model";
+import {KexLoadState} from "../../../../../core/models/kex-core.models";
 
 @Component({
   selector: 'kex-widget-experience',
@@ -10,10 +15,42 @@ import {Router} from "@angular/router";
 })
 export class KexWidgetExperienceComponent {
 
+         private limitOfExperiences = 3;
+         public userExperiences : Experience[] = [];
+         public subs : Subscription[] = [];
+         KexLoadState = KexLoadState;
   constructor(private store : Store<KexProfileState>, private router : Router){
 
   }
+
+  get hasExperiences() {
+      return this.userExperiences.length > 0;
+    }
+  get $experiences() : Observable<Experience[]>
+    {
+      return this.store.select(KexProfileSelector.getExperiences);
+    }
+
+     get $experiencesLoadState() : Observable<KexLoadState>
+      {
+        return this.store.select(KexProfileSelector.getExperiencesLoadState);
+      }
   goToProfile() {
     this.router.navigate(['profile'], {queryParams : {tab : '1'}}).then();
   }
+
+    ngOnInit(): void {
+      this.store.dispatch(GetExperiencesActions.do());
+      this.subs.push(this.$experiences.subscribe(experiences => {
+        if(experiences.length > this.limitOfExperiences){
+          this.userExperiences = experiences.slice(-this.limitOfExperiences).reverse();
+        }else{
+          this.userExperiences = experiences;
+        }
+      }))
+    }
+
+    ngOnDestroy(): void {
+      this.subs.forEach(sub => sub.unsubscribe());
+    }
 }
