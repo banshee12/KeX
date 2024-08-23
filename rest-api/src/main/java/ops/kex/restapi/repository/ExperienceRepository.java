f36077ead35da4eef1546fc2b9e6fc1f46728cd8
Jens Arth <Jens.Arth@outlook.de>
2024-08-23 09:48:43 +0200

    -changed userSkill update. Now updating experience skill as well

--- rest-api/src/main/java/ops/kex/restapi/repository/ExperienceRepository.java
+++ rest-api/src/main/java/ops/kex/restapi/repository/ExperienceRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 
 @Repository
@@ -17,4 +18,8 @@ public interface ExperienceRepository
     List<Experience> findExperiencesBySkillTitle(String title);
     List<Experience> findExperiencesByUserUserId(Sort sort, Integer userId);
     List<Experience> getExperiencesByUserUserId(Pageable pageable, Integer userId);
+
+    Optional<List<Experience>> getExperiencesBySkillTitleAndUserUserId(String SkillTitle, Integer userId);
+
+    Optional<Experience> findExperienceById(Integer experienceId);
 }
--- rest-api/src/main/java/ops/kex/restapi/service/UserSkillsService.java
+++ rest-api/src/main/java/ops/kex/restapi/service/UserSkillsService.java
@@ -24,6 +24,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -35,6 +36,7 @@ public class UserSkillsService {
     private final UserRepository userRepository;
     private final SkillsRepository skillsRepository;
     private final ExperienceRepository experienceRepository;
+    private final UserService userService;
 
 
     public ResponseEntity<List<UserSkills>> getUserSkills(SortData sortData) {
@@ -112,60 +114,74 @@ public class UserSkillsService {
         }
     }
 
-
     @Transactional
     public ResponseEntity<String> updateUserSkill(UserSkills userSkill) {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (authentication instanceof AnonymousAuthenticationToken) {
+        User loggedUser = userService.getLoggedUser();
+        if(loggedUser != null){
+            Optional<UserSkills> optionalUserSkill = userSkillsRepository.findUserSkillsById(userSkill.getId());
+            if(optionalUserSkill.isPresent()){
+                //Init
+                //check for name/skill change
+                if(!Objects.equals(optionalUserSkill.get().getSkill().getTitle(), userSkill.getSkill().getTitle())){
+                    //Save old userSkill for experience update
+                    String oldSkillTitle = optionalUserSkill.get().getSkill().getTitle();
+                    Integer oldSkillId = optionalUserSkill.get().getSkill().getId();
+                    log.info("old skill title: {}", oldSkillTitle);
+                    log.info("old skill id: {}", oldSkillId);
+                    //check if user has skill
+                    Optional<UserSkills> checkUserSkill = userSkillsRepository.findUserSkillsByUserUserIdAndSkillTitleIgnoringCase(loggedUser.getUserId(), userSkill.getSkill().getTitle());
+                    if(checkUserSkill.isEmpty()){
+                        loggedUser.removeUserSkill(optionalUserSkill.get().getId());
+                        //check if skill exist
+                        Optional<Skills> optionalSkills = skillsRepository.findSkillsByTitleIgnoreCase(userSkill.getSkill().getTitle());
+                        if(optionalSkills.isEmpty()){
+                            //save new skill
+                            Skills newSkill = Skills.builder()
+                                    .title(userSkill.getSkill().getTitle())
+                                    .build();
+                            skillsRepository.save(newSkill);
+                            optionalSkills = skillsRepository.findSkillsByTitleIgnoreCase(newSkill.getTitle());
+                            log.info("skill '{}' added to database", newSkill.getTitle());
+                        }
+                        //Update UserSkill Skill
+                        optionalUserSkill.get().setSkill(optionalSkills.get());
+                        log.info("skill '{}' added to '{}'", optionalSkills.get().getTitle(), loggedUser.getUsername());
+
+                        //update experience
+                        Optional<List<Experience>> optionalExperienceList = experienceRepository.getExperiencesBySkillTitleAndUserUserId(oldSkillTitle, loggedUser.getUserId());
+                        if(optionalExperienceList.isPresent()){
+                            if(!optionalExperienceList.get().isEmpty()){
+                                log.info("experiences needs to be updated");
+                                for(Experience experience : optionalExperienceList.get()){
+                                    experience.removeSkill(oldSkillId);
+                                    experience.addSkill(optionalSkills.get());
+                                    log.info("skill '{}' added to experience '{}'", optionalSkills.get().getTitle(), experience.getTitle());
+                                }
+                            } else {
+                                log.info("no experience affected");
+                            }
+                        }
+                    } else {
+                        log.error("user '{}' already has userSkill '{}'", loggedUser.getUsername(), userSkill.getSkill().getTitle());
+                        return new ResponseEntity<>(
+                                "no user logged in",
+                                HttpStatus.UNAUTHORIZED);
+                    }
+                } else {
+                    log.info("same skill");
+                }
+                optionalUserSkill.get().setUser(loggedUser);
+                optionalUserSkill.get().setVisible(userSkill.getVisible());
+                optionalUserSkill.get().setLevel(userSkill.getLevel());
+
+            }
+            log.info("userSkill with ID '{}' update", userSkill.getId());
+            return new ResponseEntity<>(HttpStatus.OK);
+        } else {
             log.error("no user logged in");
             return new ResponseEntity<>(
                     "no user logged in",
                     HttpStatus.UNAUTHORIZED);
-        } else {
-            if (userSkillsRepository.findUserSkillsById(userSkill.getId()).isPresent()){
-                User user = userRepository.findUserByUsernameIgnoreCase(authentication.getName());
-                if (user != null) {
-                    List<UserSkills> allUserSkills = user.getUserSkills();
-                    user.removeUserSkill(userSkill.getId());
-                    Skills newSkill = skillsRepository.findSkillByTitleIgnoreCase(userSkill.getSkill().getTitle());
-                    if (newSkill == null) {
-                        log.info("Skill " + userSkill.getSkill().getTitle() + " does not exist in database");
-                        newSkill = Skills.builder()
-                                .title(userSkill.getSkill().getTitle())
-                                .build();
-                        skillsRepository.save(newSkill);
-                        log.info("Skill " + userSkill.getSkill().getTitle() + " has been added to database");
-                    }
-                    List<User> usersThatHaveToBeAddedSkill = userRepository.findUsersByUserSkillsSkill(userSkill.getSkill());
-                    UserSkills checkUserSkill = userSkillsRepository.getUserSkillsById(userSkill.getId());
-                    if(!usersThatHaveToBeAddedSkill.contains(user) || userSkill.getSkill().getTitle().equals(checkUserSkill.getSkill().getTitle()) || usersThatHaveToBeAddedSkill.isEmpty()){
-                        user.setUserSkills(allUserSkills);
-                        userSkill.setSkill(newSkill);
-                        userSkill.setUser(user);
-                        user.addUserSkill(userSkill);
-                        userRepository.save(user);
-                        log.info("skill '" + newSkill.getTitle() + "' updated for " + user.getUsername());
-                        return new ResponseEntity<>(
-                                "skill '" + newSkill.getTitle() + "' updated for '" + user.getUsername()+ "'" ,
-                                HttpStatus.OK);
-                    } else{
-                        log.error("user " + user.getUsername() + " already has skill '" + userSkill.getSkill().getTitle()+"'");
-                        return new ResponseEntity<>(
-                                "user " + user.getUsername() + " already has skill '" + userSkill.getSkill().getTitle()+"'",
-                                HttpStatus.CONFLICT);
-                    }
-                } else {
-                    log.error("User " + authentication.getName() + " does not exist in database");
-                    return new ResponseEntity<>(
-                            "User " + authentication.getName() + " does not exist in database",
-                            HttpStatus.INTERNAL_SERVER_ERROR);
-                }
-            } else {
-                log.error("User Skill with ID " + userSkill.getId() + " does not exist in database");
-                return new ResponseEntity<>(
-                        "User Skill with ID " + userSkill.getId() + " does not exist in database",
-                        HttpStatus.CONFLICT);
-            }
         }
     }
 
