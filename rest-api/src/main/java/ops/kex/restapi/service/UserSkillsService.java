c27543e34e6a1c92a747d6413754724951cc94fe
Jens Arth <Jens.Arth@outlook.de>
2024-08-24 12:21:12 +0200

    -fixed branch (hopeyfully)

--- rest-api/src/main/java/ops/kex/restapi/service/UserSkillsService.java
+++ rest-api/src/main/java/ops/kex/restapi/service/UserSkillsService.java
@@ -22,7 +22,6 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
